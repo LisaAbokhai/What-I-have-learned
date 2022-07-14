f840ddf817fb6e9affcfa31831589a320ea29473
lisa <abokhailisa@gmail.com>
2022-07-14 21:40:41 +0100

    Api practice project

---
+++ Extracting_data_from__an_API/code/extract_from_mal.py
@@ -0,0 +1,77 @@
+#  request to get response from api
+# json to load json
+#  Configparser to pull clientinfo
+# pandas to load dataframe into a csv
+
+import requests
+import json
+from configparser import ConfigParser
+import pandas as pd
+
+#  'My anime list' api url
+api_url = 'https://api.myanimelist.net/v2/anime/ranking'
+
+#  Filter by favorite anime in anime rank and limit by 200
+ranking_type = 'favorite'
+limit = 300
+rank_params = {'ranking_type' : ranking_type, 'limit' : limit}
+
+# Read pipeline cinfiguration file
+parser = ConfigParser()
+parser.read(r'Extracting_data_from__an_API\code\sensitive.conf')
+
+#  user agent for headers
+client = parser.get("api_client_detail", "client_id")
+auth = {'X-MAL-CLIENT-ID' : client }
+
+
+#  Combine dicitionaries to form one
+def Merge(dict1, dict2, dict3):
+        """_summary_
+
+        Args:
+            dict1 (dictionary): single key and value
+            dict2 (dictionary): single key and value
+            dict3 (dictionary): single key and value
+
+        Returns:
+            dictionary: combination of all 3 dictionaries into 1 dictionary
+        """
+        res = {**dict1, **dict2, **dict3}
+        return res
+
+
+def main():
+
+        api_response = requests.get(api_url, params = rank_params, headers= auth)   
+        json_response = json.loads(api_response.content)
+
+       
+        #  Store values gotten from api in this var
+        anime_rank = []
+
+        #  Loop through data got get values
+        for anime_info in json_response['data']:
+
+                #  Get the important from the json response
+                id  = {'id' : anime_info['node']['id']}
+                title = {'title' : anime_info['node']['title']}
+                rank = anime_info['ranking']
+
+                #  Merge the dictionaries to parse them as one unit
+                add_dict = Merge(id, title, rank)     
+
+                # Add to dictionary to list
+                anime_rank.append(add_dict)
+              
+
+
+        anime_dataframe = pd.DataFrame(anime_rank)
+        anime_dataframe.index.name = 'index'  # create index coloumn name
+        print(anime_dataframe.head())
+        anime_dataframe.to_csv(r'Extracting_data_from__an_API\csv\anime_favorite_rank.csv', sep = '|')
+
+
+if __name__ == '__main__':
+        main()
+
---
+++ Extracting_data_from__an_API/code/load_into_s3.py
@@ -0,0 +1,39 @@
+#  boto3 for connection with aws
+#  configparser to get sensitive data from pipiline.conf file
+#  to_csv to get the variable for the csv
+import boto3
+import configparser
+
+#  Parse sql.conf file
+parser = configparser.ConfigParser()
+path = r'Extracting_data_from__an_API\code\sensitive.conf'
+parser.read(path)
+
+#  Create environment vars
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+
+
+#  Connect to s3 and create bucket
+s3 = boto3.client('s3',
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key)
+
+s3.create_bucket(Bucket='mal-api-anime-bucket')
+
+#  Upload the csv into s3 bucket
+path = r'Extracting_data_from__an_API\csv\anime_favorite_rank.csv'
+s3_file = 'anime_rank_300'
+bucket_name = 'mal-api-anime-bucket'
+s3.upload_file(path, bucket_name, s3_file)
+
+# Check if bucket has been created
+# Call S3 to list current buckets
+response = s3.list_buckets()
+
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Print out the bucket list
+print(f"Bucket List: {buckets}")
+
